c76c29d05fde7f641afa28aab8340fcb77a13dd7
Mark Tasaka <mark.tasaka@gmail.com>
2021-10-04 12:57:30 -0700

    Update 1.01

--- sword/php/magicSword.php
+++ sword/php/magicSword.php
@@ -7,4 +7,183 @@ function getDieRoll ()
     return $roll;
 }
 
+function getSwordType()
+{
+    $number = rand(1, 100);
+
+    if($number <= 15)
+    {
+        return 'Dagger';
+    }
+    else if($number >= 16 && $number <= 30)
+    {
+        return 'Short Sword';
+    }
+    else if($number >= 31 && $number <= 80)
+    {
+        return 'Longsword';
+    }
+    else
+    {
+        return 'Two-handed Sword';
+    }
+}
+
+
+function getSwordAlignment()
+{
+    $number = rand(1, 100);
+
+    if($number <= 40)
+    {
+        return 'Chaotic';
+    }
+    else if($number >= 41 && $number <= 60)
+    {
+        return 'Neutral';
+    }
+    else
+    {
+        return 'Lawful';
+    }
+}
+
+function getToHitBonus($input)
+{
+    if($input <= 90)
+    {
+        return '+1';
+    }
+    else if($input >= 91 && $input <= 96)
+    {
+        return '+2';
+    }
+    else if($input >= 97 && $input <= 98)
+    {
+        $number = rand(1, 100);
+
+        if($number <= 75)
+        {
+            return '+3';
+        }
+        else
+        {
+            return '+4';
+        }
+    }
+    else
+    {
+        $number = rand(1, 100);
+        if($number <= 50)
+        {
+            return '+3';
+        }
+        else if($number >= 51 && $number <= 85)
+        {
+            return '+4';
+        }
+        else
+        {
+            return '+5';
+        }
+    }
+
+}
+
+
+function getSwordIntelligence($input)
+{
+    if($input <= 50)
+    {
+        $int = rand(1, 6);
+        $roll = ' (1d6)';
+    }
+    else if($input >= 51 && $input <= 75)
+    {
+        $int = rand(1, 6);
+        $int += 2;
+        $roll = ' (1d6+2)';
+    }
+    else if($input >= 76 && $input <= 85)
+    {
+        $die1 = rand(1, 4);
+        $die2 = rand(1, 4);
+        $die3 = rand(1, 4);
+        $int = $die1 + $die2 + $die3;
+        $roll = ' (3d4)';
+    }
+    else if($input >= 86 && $input <= 90)
+    {
+        $die1 = rand(1, 6);
+        $die2 = rand(1, 6);
+        $int = $die1 + $die2;
+        $roll = ' (2d6)';
+    }
+    else if($input >= 91 && $input <= 94)
+    {
+        $die1 = rand(1, 6);
+        $die2 = rand(1, 6);
+        $int = $die1 + $die2 + 2;
+        $roll = ' (2d6+2)';
+    }
+    else if($input >= 95 && $input <= 96)
+    {
+        $die1 = rand(1, 6);
+        $die2 = rand(1, 6);
+        $die3 = rand(1, 6);
+        $int = $die1 + $die2 + $die3;
+        $roll = ' (3d6)';
+    }
+    else if($input >= 97 && $input <= 98)
+    {
+        $die1 = rand(1, 6);
+        $die2 = rand(1, 6);
+        $die3 = rand(1, 6);
+        $int = $die1 + $die2 + $die3 + 2;
+        $roll = ' (3d6+2)';
+    }
+    else
+    {
+        $die1 = rand(1, 6);
+        $die2 = rand(1, 6);
+        $die3 = rand(1, 6);
+        $int = $die1 + $die2 + $die3 + 4;
+        $roll = ' (3d6+4)';
+    }
+
+    $inteligence = array($int, $roll);
+    return $inteligence;
+}
+
+
+function getSwordCommunication($input)
+{
+    if($input <= 50)
+    {
+        return 'None';
+    }
+    else if($input >= 51 && $input <= 85)
+    {
+        return 'Simple urges';
+    }
+    else if($input >= 86 && $input <= 94)
+    {
+        return 'Empathy';
+    }
+    else if($input >= 95 && $input <= 96)
+    {
+        return 'Speech';
+    }
+    else if($input >= 97 && $input <= 98)
+    {
+        return 'Telepathy';
+    }
+    else
+    {
+        return 'Speech & telepathy';
+    }
+
+}
+
+
 ?>
--- sword/sword.php
+++ sword/sword.php
@@ -30,6 +30,36 @@ $dieRoll1 = getDieRoll();
 $dieRoll2 = getDieRoll();
 $dieRoll3 = getDieRoll();
 
+$swordToHit0 = getToHitBonus($dieRoll0);
+$swordToHit1 = getToHitBonus($dieRoll1);
+$swordToHit2 = getToHitBonus($dieRoll2);
+$swordToHit3 = getToHitBonus($dieRoll3);
+
+$swordType0 = getSwordType();
+$swordType1 = getSwordType();
+$swordType2 = getSwordType();
+$swordType3 = getSwordType();
+
+$swordAlignment0 = getSwordAlignment();
+$swordAlignment1 = getSwordAlignment();
+$swordAlignment2 = getSwordAlignment();
+$swordAlignment3 = getSwordAlignment();
+
+$swordIntArray0 = array();
+$swordIntArray1 = array();
+$swordIntArray2 = array();
+$swordIntArray3 = array();
+
+$swordIntArray0 = getSwordIntelligence($dieRoll0);
+$swordIntArray1 = getSwordIntelligence($dieRoll1);
+$swordIntArray2 = getSwordIntelligence($dieRoll2);
+$swordIntArray3 = getSwordIntelligence($dieRoll3);
+
+$swordCommunication0 = getSwordCommunication($dieRoll0);
+$swordCommunication1 = getSwordCommunication($dieRoll1);
+$swordCommunication2 = getSwordCommunication($dieRoll2);
+$swordCommunication3 = getSwordCommunication($dieRoll3);
+
 
 ?>
 
@@ -39,6 +69,31 @@ $dieRoll3 = getDieRoll();
 
    
 <aside id="topleft">
+
+
+<span id="swordType0">
+        <?php
+            echo $swordToHit0 . ' ' . $swordType0;
+            ?>
+    </span>
+
+    
+<span id="swordAlignment0">
+        <?php
+            echo $swordAlignment0;
+            ?>
+    </span>
+
+        
+    <span id="swordIntelligence0">
+        <?php
+        foreach($swordIntArray0 as $int)
+        {
+            echo $int;
+        }
+            ?>
+    </span>
+	
 	
 	<span id="dieRoll0">
         <?php
@@ -54,6 +109,30 @@ $dieRoll3 = getDieRoll();
 
 
 <aside id="topright">
+
+
+<span id="swordType1">
+        <?php
+            echo $swordToHit1 . ' ' . $swordType1;
+            ?>
+    </span>
+    
+    
+<span id="swordAlignment1">
+        <?php
+            echo $swordAlignment1;
+            ?>
+    </span>
+
+            
+    <span id="swordIntelligence1">
+        <?php
+        foreach($swordIntArray1 as $int)
+        {
+            echo $int;
+        }
+            ?>
+    </span>
 		
 	<span id="dieRoll1">
         <?php
@@ -71,6 +150,32 @@ $dieRoll3 = getDieRoll();
 
 
 <aside id="bottomleft">
+
+
+
+<span id="swordType2">
+        <?php
+            echo $swordToHit2 . ' ' . $swordType2;
+            ?>
+    </span>
+    
+    
+<span id="swordAlignment2">
+        <?php
+            echo $swordAlignment2;
+            ?>
+    </span>
+
+            
+    <span id="swordIntelligence2">
+        <?php
+        foreach($swordIntArray2 as $int)
+        {
+            echo $int;
+        }
+            ?>
+    </span>
+		
 			
 	<span id="dieRoll2">
         <?php
@@ -84,6 +189,32 @@ $dieRoll3 = getDieRoll();
 
 
 <aside id="bottomright">
+
+
+
+<span id="swordType3">
+        <?php
+            echo $swordToHit3 . ' ' . $swordType3;
+            ?>
+    </span>
+    
+    
+<span id="swordAlignment3">
+        <?php
+            echo $swordAlignment3;
+            ?>
+    </span>
+
+            
+    <span id="swordIntelligence3">
+        <?php
+        foreach($swordIntArray3 as $int)
+        {
+            echo $int;
+        }
+            ?>
+    </span>
+		
 				
 	<span id="dieRoll3">
         <?php
